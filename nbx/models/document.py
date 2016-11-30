67e1601015f380126aa470721bb7b0158e10f81e
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-11-30 12:36:31 -0300

    Some work on Document model

--- nbx/models/document.py
+++ nbx/models/document.py
@@ -7,18 +7,44 @@ from nbx.models.misc import TimestampMixin
 
 class Document(db.Model, TimestampMixin):
     __tablename__ = 'document'
+
+    TYPE_FACTURA_A = 'TYPE_FACTURA_A'
+    TYPE_NOTA_CREDITO_A = 'TYPE_NOTA_CREDITO_A'
+    TYPE_PRESUPUESTO = 'TYPE_PRESUPUESTO'
+
+    _doc_type = {
+        TYPE_FACTURA_A: 'Factura A',
+        TYPE_NOTA_CREDITO_A: 'Nota de Crédito',
+        TYPE_PRESUPUESTO: 'Presupuesto',
+    }
+
+    STATUS_PENDING = 'STATUS_PENDING'
+    STATUS_EXPIRED = 'STATUS_EXPIRED'
+    STATUS_PAID = 'STATUS_PAID'
+
+    _doc_status = {
+        STATUS_PENDING: 'Pendiente',
+        STATUS_EXPIRED: 'Vencida',
+        STATUS_PAID: 'Pagada',
+    }
+
     id = db.Column(db.Integer, primary_key=True)
-    doc_type = db.Column(db.Unicode(10), nullable=False)
+    doc_type = db.Column(db.Enum(*_doc_type.keys(), name='doc_type'),
+                         default=TYPE_FACTURA_A)
     point_sale = db.Column(db.Integer, nullable=False)
     number = db.Column(db.Integer, nullable=False)
     total = db.Column(db.Numeric(10, 2), nullable=False)
-    _state = db.Column("state", db.Enum(u'EXPIRED', u'PENDING', u'PAID', name=u'State'), default=u'PENDING')
+    doc_status = db.Column(db.Enum(*_doc_status.keys(), name='doc_status'),
+                       default=STATUS_PENDING)
     issue_date = db.Column(db.Date)
     expiration_date = db.Column(db.Date)
     notes = db.Column(db.UnicodeText)
-    supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.supplier_id'), nullable=False)
+    supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.supplier_id'),
+                            nullable=False)
 
-    document_payments = db.relationship('DocumentPayment', cascade='all, delete-orphan', backref="document")
+    document_payments = db.relationship('DocumentPayment',
+                                        cascade='all, delete-orphan',
+                                        backref="document")
 
     def add_payment(self, payment, amount=None):
         if amount is None:
@@ -26,12 +52,20 @@ class Document(db.Model, TimestampMixin):
         self.document_payments.append(DocumentPayment(payment, amount))
 
     @property
+    def type(self):
+        return self._doc_type.get(self.doc_type)
+
+    @property
+    def status(self):
+        return self._doc_status.get(self.doc_status)
+
+    @property
     def fulldesc(self):
         return u"%s %04d-%08d" % (self.doc_type, self.point_sale, self.number)
 
     @property
     def cancelled_date(self):
-        if self.state in (u'PENDING', u'EXPIRED'):
+        if self.state in (self.STATUS_PENDING, self.STATUS_EXPIRED):
             return None
         return Payment.query.join('document_payment')\
                             .filter(DocumentPayment.document==self)\
@@ -40,5 +74,6 @@ class Document(db.Model, TimestampMixin):
     @property
     def balance(self):
         paid = db.session.query(func.sum(DocumentPayment.amount))\
-                         .filter(DocumentPayment.invoice==self).scalar() or Decimal(0)
+                         .filter(DocumentPayment.invoice==self).scalar()\
+                         or Decimal(0)
         return self.total - paid
