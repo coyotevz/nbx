43f0f3a9cb1c60fd473abb2861b9edf536377ba9
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-12-15 10:48:19 -0300

    Add Expose headers to CORS

--- nbx/application.py
+++ nbx/application.py
@@ -72,6 +72,7 @@ def configure_app(app, config=None):
             a('Access-Control-Allow-Credentials', 'true')
             a('Access-Control-Allow-Headers', 'Content-Type,Authorization')
             a('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+            a('Access-Control-Expose-Headers', 'Link,X-Total-Count')
         return response
 
 
