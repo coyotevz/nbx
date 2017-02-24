69d0ec6a1dba89975092024d3158fa95c05bcb79
Augusto Roccasalva <augusto@rioplomo.com.ar>
2017-02-24 02:11:07 -0300

    Add some fields to document api

--- nbx/api/document.py
+++ nbx/api/document.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from flask_potion import ModelResource, fields
+from flask_potion.contrib.alchemy.fields import InlineModel
 
-from nbx.models import Document
+from nbx.models import Document, Supplier
 
 
 DocumentSchema = fields.Object({
@@ -25,6 +26,11 @@ class DocumentResource(ModelResource):
         name = 'documents'
 
     class Schema:
+        supplier = fields.ToOne('suppliers')
+        supplier_data = InlineModel({
+            '$id': fields.Integer(attribute='id'),
+            'rz': fields.String(),
+        }, model=Supplier, attribute='supplier')
         type = fields.String(io='r')
         short_type = fields.String(io='r')
         full_number = fields.String(io='r')
